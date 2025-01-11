fe3bec4e74dc0daef1c6c5cca07bf716ff528202
Oky Aneka <okyaneka@gmail.com>
2025-01-11 11:26:12 +0700

    feat: enhance UserPublicService verification and setup methods for improved user handling

--- _src/services/UserPublicService/index.ts
+++ _src/services/UserPublicService/index.ts
@@ -1,19 +1,37 @@
 import { enc, lib, SHA256 } from "crypto-js";
+import { UserModel } from "~/models";
 import UserPublic from "~/models/UserPublicModel";
 
-export const verify = async (code: string) => {
-  const user = await UserPublic.findOne({ code, deletedAt: null });
-  if (!user) throw new Error("code invalid");
-  user.lastAccessedAt = new Date();
-  await user.save();
-  return user.toObject();
+export const verify = async (value: string) => {
+  const userPublic = await UserPublic.findOneAndUpdate(
+    {
+      $or: [{ "user.id": value }, { code: value }],
+      deletedAt: null,
+    },
+    { lastAccessedAt: new Date() }
+  );
+
+  if (!userPublic) throw new Error("invalid user");
+  return userPublic.toObject();
 };
 
-export const setup = async () => {
+export const setup = async (userId?: string) => {
   const timestamp = Date.now();
   const salt = lib.WordArray.random(4).toString(enc.Hex);
   const code: string = SHA256(`${timestamp}${salt}`).toString(enc.Hex);
-  const user = await UserPublic.create({ code });
+  const payload: any = { code };
+
+  if (userId) {
+    const userPublic = await UserPublic.findOne({
+      "user.id": userId,
+      deletedAt: null,
+    });
+    if (userPublic) return userPublic.toObject();
+    const user = await UserModel.findOne({ _id: userId, deletedAt: null });
+    if (user) payload.user = { id: user.id, name: user.name };
+  }
+
+  const user = await UserPublic.create(payload);
   return user.toObject();
 };
 
