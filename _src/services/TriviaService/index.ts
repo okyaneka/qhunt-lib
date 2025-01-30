69a565e36c485ce4240b64da45f00212f8794cb0
Oky Aneka <okyaneka@gmail.com>
2025-01-30 19:23:42 +0700

    refactor: update TriviaService and ChallengeService for improved functionality and code clarity

--- _src/services/TriviaService/index.ts
+++ _src/services/TriviaService/index.ts
@@ -1,55 +1,87 @@
-import { Challenge } from "~/models/ChallengeModel";
-import Trivia, { TriviaPayload } from "~/models/TriviaModel";
+import TriviaModel, { Trivia, TriviaPayload } from "~/models/TriviaModel";
 import ChallengeService from "../ChallengeService";
+import { deepmerge } from "~/helpers/common";
 
-export const sync = async (
-  challenge: Challenge,
-  items: TriviaPayload[]
-): Promise<string[]> => {
-  const idName = { id: challenge.id, name: challenge.name };
-  const create = items
-    .filter((item) => !item.id)
-    .map((item) => ({ ...item, challenge: idName }));
-  const update = items.filter((item) => item.id);
+const createMany = async (challengeId: string, payload: TriviaPayload[]) => {
+  if (payload.length === 0) return [];
 
-  await Trivia.updateMany(
-    { "challenge.id": challenge.id },
-    { $set: { challenge: null } }
-  );
+  const challenge = await ChallengeService.detail(challengeId);
+  const challengeForeign = { id: challenge.id, name: challenge.name };
 
-  const actCreate = Trivia.insertMany(create);
-  const actUpdate = update.map((item) =>
-    Trivia.findOneAndUpdate({ _id: item.id }, { $set: item }, { new: true })
-  );
+  // FOR NEXT DEVELOPMENT IF ITS ENABLE SINGULAR TRIVIA
+  // const qrParams = await QrListParamsValidator.validateAsync({
+  //   hasContent: false,
+  //   limit: payload.length,
+  // });
+  // const qrs = (await QrService.list(qrParams)).list.map(({ id, code }) => ({
+  //   id,
+  //   code,
+  // }));
+  // if (qrs.length !== payload.length)
+  //   throw new Error("QR code data is not enough. please generate again.");
 
-  const [resCreate, ...resUpdate] = await Promise.all([
-    actCreate,
-    ...actUpdate,
-  ]);
-
-  const content = resUpdate
-    .map((item) => item?._id.toString())
-    .concat(...resCreate.map((item) => item._id.toString()))
-    .filter((v) => v != undefined);
-
-  await ChallengeService.updateContent(challenge.id, content);
-
-  return content;
+  const items = payload.map((item, i) => ({
+    ...item,
+    challenge: challengeForeign,
+  }));
+  return await TriviaModel.insertMany(items);
 };
 
-export const content = async (challenge: Challenge) => {
-  const items = await Trivia.find({ _id: { $in: challenge.contents } });
-  return items.map((item) => item.toObject());
+const updateMany = async (payload: TriviaPayload[]) => {
+  if (payload.length === 0) return [];
+  const ids = payload.map(({ id }) => id);
+  const existing = await TriviaModel.find({ _id: { $in: ids } });
+  const newValue = existing.map((old) => {
+    const val = payload.find((val) => val.id == old.id);
+    return deepmerge<Trivia>(old.toObject(), val || {});
+  });
+
+  await TriviaModel.bulkWrite(
+    newValue.map((item) => {
+      return {
+        updateOne: {
+          filter: { _id: item.id },
+          update: { $set: item },
+        },
+      };
+    })
+  );
+  return await TriviaModel.find({ _id: { $in: ids } });
 };
 
 export const detail = async (id: string) => {
-  const item = await Trivia.findOne({ _id: id });
+  const item = await TriviaModel.findOne({ _id: id });
   if (!item) throw new Error("trivia not found");
   return item;
 };
 
+export const details = async (challengeId: string) => {
+  const items = await TriviaModel.find({ "challenge.id": challengeId });
+  return items.map((item) => item.toObject());
+};
+
+export const sync = async (challengeId: string, payload: TriviaPayload[]) => {
+  if (payload.length === 0) return [];
+
+  const { create: itemsCreate, update: itemsUpdate } = payload.reduce<{
+    create: TriviaPayload[];
+    update: TriviaPayload[];
+  }>(
+    (acc, cur) => {
+      acc[cur.id ? "update" : "create"].push(cur);
+      return acc;
+    },
+    { create: [], update: [] }
+  );
+
+  const itemsCreated = await createMany(challengeId, itemsCreate);
+  const itemsUpdated = await updateMany(itemsUpdate);
+
+  return [...itemsCreated, ...itemsUpdated].map((item) => item.toObject());
+};
+
 export const verify = async (id: string) => {};
 
-const TriviaService = { sync, content, detail, verify };
+const TriviaService = { detail, details, sync, verify };
 
 export default TriviaService;
