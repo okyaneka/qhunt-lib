35956c244ae2174b9e960c9e5b9cc913cc3907d9
Oky Aneka <okyaneka@gmail.com>
2025-02-25 23:57:59 +0700

    feat: add S3 service and helper for file management with AWS S3 integration

---
+++ _src/types/s3/index.ts
@@ -0,0 +1,19 @@
+import { Timestamps } from "~/helpers/types";
+import { UserForeign } from "../user";
+
+export interface S3Payload {
+  buffer: Buffer;
+  mimetype: string;
+  filename: string;
+}
+
+export type S3Foreign = Pick<S3, "fileName" | "fileUrl" | "fileSize">;
+
+export interface S3 extends Timestamps {
+  id: string;
+  fileName: string;
+  fileUrl: string;
+  fileType: string;
+  fileSize: number;
+  user: UserForeign;
+}
