bba9732e9cca94376044d6d5423c002b9d6f80c6
Oky Aneka <okyaneka@gmail.com>
2025-02-25 23:58:20 +0700

    feat: integrate S3Helper and enhance RedisHelper with improved subscription handling

--- _src/plugins/redis/types.ts
+++ _src/plugins/redis/types.ts
@@ -2,7 +2,10 @@ import { ValueOf } from "~/helpers/types";
 
 const REDIS_KEYS = {} as const;
 
-const REDIS_CHANNELS = { UpdateUser: "update-user" } as const;
+const REDIS_CHANNELS = {
+  Leaderboard: "leaderboard",
+  UpdateUser: "update-user",
+} as const;
 
 export type RedisKey = ValueOf<typeof REDIS_KEYS>;
 export type RedisChannel = ValueOf<typeof REDIS_CHANNELS>;
